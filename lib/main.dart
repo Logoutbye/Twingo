2a7161c14b14aa471bab4743b9bcbfe4b4fd4978
logoutbye <muhammadbilal03478@gmail.com>
2023-07-25 13:14:39 +0500

    pulli

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Twingo',
+        title: 'Twigo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
