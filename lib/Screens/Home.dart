edea52296549d45bd4c15578c4ef439bf5efe031
logoutbye <muhammadbilal03478@gmail.com>
2023-07-28 14:26:52 +0500

    facebookc ads not workinh

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -5,7 +5,7 @@ import 'package:ezeehome_webview/Contrlller/InternetConnectivity.dart';
 import 'package:ezeehome_webview/constants.dart';
 import 'package:facebook_audience_network/facebook_audience_network.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_pro/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -21,45 +21,36 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  var IsInternetConnected = true;
-  bool loader = false;
-  final _webViewController = Completer<WebViewController>();
-  double _progress = 0.0; // Variable to hold the progress percentage
-  bool _isLoading = true;
-  int _progressText = 0; // Variable to track loading state
+  late InAppWebViewController _webViewController;
+  late PullToRefreshController pullToRefreshController;
+  final InAppBrowser browser = InAppBrowser();
 
-  FacebookBannerAd? facebookBannerAd;
-  //FacebookInterstitialAd? facebookInterstitialAd;
-  bool _isInterstitialAdLoaded = false;
+  bool? _isRefreshing;
 
-  // late BannerAd _bannerAd;
-  // InterstitialAd? _interstialAd;
-  // bool isAdLoaded = false;
-  // void _createBannerAd() {
-  //   _bannerAd = BannerAd(
-  //       size: AdSize.banner,
-  //       adUnitId: AdsMobServices.BannerAdUnitId!,
-  //       listener: AdsMobServices.bannerAdListener,
-  //       request: AdRequest())
-  //     ..load();
-  // }
-
-  // void _createInterstitialAd() {
-  //   InterstitialAd.load(
-  //       adUnitId: AdsMobServices.InterstitialAdId!,
-  //       request: AdRequest(),
-  //       adLoadCallback: InterstitialAdLoadCallback(
-  //           onAdLoaded: (ad) => _interstialAd = ad,
-  //           onAdFailedToLoad: (LoadAdError loadAdError) =>
-  //               _interstialAd = null));
-  // }
+  double? progress;
 
   @override
   void initState() {
+    super.initState();
+
+    pullToRefreshController = PullToRefreshController(
+      options: PullToRefreshOptions(
+        color: Colors.black,
+      ),
+      onRefresh: () async {
+        if (Platform.isAndroid) {
+          browser.webViewController.reload();
+          print("refreshing");
+        } else if (Platform.isIOS) {
+          browser.webViewController.loadUrl(
+              urlRequest:
+                  URLRequest(url: await browser.webViewController.getUrl()));
+        }
+      },
+    );
+    browser.pullToRefreshController = pullToRefreshController;
     checkInternetConnectionForDashboard();
-    if (Platform.isAndroid) {
-      WebView.platform = SurfaceAndroidWebView();
-    }
+
     requestPermissions();
     FacebookAudienceNetwork.init(
         //testingId: "a77955ee-3304-4635-be65-81029b0f5201", //optional
@@ -76,26 +67,33 @@ class _HomeState extends State<Home> {
         _loadInterstitialAd();
       });
     }
-    // _createBannerAd();
-    // _createInterstitialAd();
   }
+
+  String url = 'https://twigo.date/';
+
+  var IsInternetConnected = true;
+  bool loader = false;
+  // final _webViewController = Completer<WebViewController>();
+  double _progress = 0.0; // Variable to hold the progress percentage
+  bool _isLoading = true;
+  int _progressText = 0; // Variable to track loading state
+
+  FacebookBannerAd? facebookBannerAd;
+  bool _isInterstitialAdLoaded = false;
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
         // _showInterstitalAd();
-
-        bool? goBack =
-            await _webViewController.future.then((value) => value.canGoBack());
-        if (goBack != true) {
-          print('appClosed');
-          return true;
-        } else {
-          _webViewController.future.then((controller) => controller.goBack());
-          print('appnotClosed');
-          return false;
+        if (_webViewController != null) {
+          bool canGoBack = await _webViewController.canGoBack();
+          if (canGoBack) {
+            _webViewController.goBack();
+            return false;
+          }
         }
+        return true;
       },
       child: Scaffold(
         appBar: PreferredSize(
@@ -107,6 +105,7 @@ class _HomeState extends State<Home> {
         ),
         body: IsInternetConnected == false
             ? RefreshIndicator(
+                color: MyColors.ksecondaryColor,
                 onRefresh: () {
                   return Future.delayed(Duration(seconds: 1), () {
                     Navigator.pop(context);
@@ -128,7 +127,7 @@ class _HomeState extends State<Home> {
                           Icon(
                             Icons.signal_wifi_connected_no_internet_4,
                             size: 60,
-                            color: MyColors.kprimaryColor,
+                            color: MyColors.ksecondaryColor,
                           ),
                           Container(
                             child: Text(
@@ -153,7 +152,10 @@ class _HomeState extends State<Home> {
                                 ),
                               ));
                             },
-                            child: Text('Reload Page'),
+                            child: Text(
+                              'Reload Page',
+                              style: TextStyle(color: MyColors.ksecondaryColor),
+                            ),
                           ),
                         ],
                       ),
@@ -163,72 +165,149 @@ class _HomeState extends State<Home> {
               )
             : Stack(
                 children: [
-                  WebView(
-                    initialUrl: 'https://twigo.date/',
-                    javascriptMode: JavascriptMode.unrestricted,
-                    onWebViewCreated: (WebViewController webViewController) {
-                      _webViewController.complete(webViewController);
+               
+               
+               
+                  InAppWebView(
+                    initialUrlRequest: URLRequest(url: Uri.parse('$url')),
+                    pullToRefreshController: PullToRefreshController(
+                        options: PullToRefreshOptions(
+                            color: MyColors.ksecondaryColor),
+                        onRefresh: () {
+                          // return Future.delayed(Duration(seconds: 1), () {
+                          Navigator.pop(context);
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => Home(
+                              isInternetConnected: IsInternetConnected,
+                            ),
+                          ));
+                          // });
+                        }),
+                    initialOptions: InAppWebViewGroupOptions(
+                        crossPlatform: InAppWebViewOptions(
+                          javaScriptEnabled: true,
+                          useOnDownloadStart: true,
+                          useShouldOverrideUrlLoading: true,
+                        ),
+                        ios: IOSInAppWebViewOptions(),
+                        android: AndroidInAppWebViewOptions(
+                            useHybridComposition: true)),
+                    onWebViewCreated: (controller) {
+                      _webViewController = controller;
                     },
-                    javascriptChannels: <JavascriptChannel>{
-                      // _toasterJavascriptChannel(context),
-                      JavascriptChannel(
-                          name: 'Toaster',
-                          onMessageReceived: (JavascriptMessage message) {
-                            var snackBar = SnackBar(
-                              content: Text(message.message),
-                            );
-                            ScaffoldMessenger.of(context)
-                                .showSnackBar(snackBar);
-                          })
+                    androidOnPermissionRequest:
+                        (controller, origin, resources) async {
+                      return PermissionRequestResponse(
+                        resources: resources,
+                        action: PermissionRequestResponseAction
+                            .GRANT, // Grant camera permission
+                      );
                     },
-                    // navigationDelegate: (NavigationRequest request) {
-                    //   if (request.url.contains('https://twigo.date/')) {
-                    //     return NavigationDecision.navigate;
-                    //   } else if (request.url
-                    //       .startsWith('https://www.youtube.com/')) {
-                    //     print('blocking navigation to $request}');
-                    //     return NavigationDecision.prevent;
-                    //   } else {
-                    //     print('opening external link');
-                    //     // _launchExternalUrl(request.url);
-                    //     // launchUrl(Uri.parse(request.url));
-                    //     return NavigationDecision.navigate;
-                    //   }
-                    //   print('allowing navigation to $request');
-                    //   // return NavigationDecision.navigate;
-                    // },
-                    onProgress: (int progress) {
-                      print("WebView is loading (progress : $progress%)");
+                    onLoadStart: (controller, url) {
                       setState(() {
-                        _progress = progress / 100;
-                        _progressText = progress;
-                        // Update progress based on the value received (0-100)
-                        print("::::$_progress");
-                        if (_progress > 0.7) {
-                          setState(() {
-                            _isLoading = false;
-                          });
+                        this.url = url?.toString() ?? '';
+                      });
+                    },
+                    onLoadStop: (controller, url) async {
+                      setState(() {
+                        this.url = url?.toString() ?? '';
+                      });
+                    },
+                    onProgressChanged: (controller, progress) {
+                      setState(() {
+                        this.progress = progress / 100;
+                        if (progress == 100) {
+                          _isRefreshing = false;
                         }
                       });
                     },
-                    onPageStarted: (String url) {
-                      print('Page started loading: $url');
-                      setState(() {
-                        _isLoading =
-                            true; // Set loading state to true when a new page starts loading
-                      });
+                    shouldOverrideUrlLoading:
+                        (controller, navigationAction) async {
+                      var uri = navigationAction.request.url;
+                      if (uri!.toString().startsWith('https://twigo')) {
+                        return NavigationActionPolicy.ALLOW;
+                      } else if (uri
+                          .toString()
+                          .startsWith('https://www.youtube.com/')) {
+                        print('Blocking navigation to $uri');
+                        return NavigationActionPolicy.CANCEL;
+                      } else {
+                        print('Opening external link: $uri');
+                        // You can handle other links here and decide how to navigate to them
+                        return NavigationActionPolicy.ALLOW;
+                      }
                     },
-                    onPageFinished: (String url) {
-                      print('Page finished loading: $url');
-                      // setState(() {
-                      //   _isLoading =
-                      //       false; // Set loading state to false when the page finishes loading
-                      // });
+                    onLoadError: (controller, url, code, message) {
+                      // Handle web page load errors here
                     },
-                    gestureNavigationEnabled: true,
-                    geolocationEnabled: false,
-                    zoomEnabled: true,
-                  ),
+                  )
+
+                  // WebView(
+                  //   initialUrl: 'https://twigo.date/',
+                  //   javascriptMode: JavascriptMode.unrestricted,
+                  //   onWebViewCreated: (WebViewController webViewController) {
+                  //     _webViewController.complete(webViewController);
+                  //   },
+                  //   javascriptChannels: <JavascriptChannel>{
+                  //     // _toasterJavascriptChannel(context),
+                  //     JavascriptChannel(
+                  //         name: 'Toaster',
+                  //         onMessageReceived: (JavascriptMessage message) {
+                  //           var snackBar = SnackBar(
+                  //             content: Text(message.message),
+                  //           );
+                  //           ScaffoldMessenger.of(context)
+                  //               .showSnackBar(snackBar);
+                  //         })
+                  //   },
+                  //   // navigationDelegate: (NavigationRequest request) {
+                  //   //   if (request.url.contains('https://twigo.date/')) {
+                  //   //     return NavigationDecision.navigate;
+                  //   //   } else if (request.url
+                  //   //       .startsWith('https://www.youtube.com/')) {
+                  //   //     print('blocking navigation to $request}');
+                  //   //     return NavigationDecision.prevent;
+                  //   //   } else {
+                  //   //     print('opening external link');
+                  //   //     // _launchExternalUrl(request.url);
+                  //   //     // launchUrl(Uri.parse(request.url));
+                  //   //     return NavigationDecision.navigate;
+                  //   //   }
+                  //   //   print('allowing navigation to $request');
+                  //   //   // return NavigationDecision.navigate;
+                  //   // },
+                  //   onProgress: (int progress) {
+                  //     print("WebView is loading (progress : $progress%)");
+                  //     setState(() {
+                  //       _progress = progress / 100;
+                  //       _progressText = progress;
+                  //       // Update progress based on the value received (0-100)
+                  //       print("::::$_progress");
+                  //       if (_progress > 0.7) {
+                  //         setState(() {
+                  //           _isLoading = false;
+                  //         });
+                  //       }
+                  //     });
+                  //   },
+                  //   onPageStarted: (String url) {
+                  //     print('Page started loading: $url');
+                  //     setState(() {
+                  //       _isLoading =
+                  //           true; // Set loading state to true when a new page starts loading
+                  //     });
+                  //   },
+                  //   onPageFinished: (String url) {
+                  //     print('Page finished loading: $url');
+                  //     // setState(() {
+                  //     //   _isLoading =
+                  //     //       false; // Set loading state to false when the page finishes loading
+                  //     // });
+                  //   },
+                  //   gestureNavigationEnabled: true,
+                  //   geolocationEnabled: false,
+                  //   zoomEnabled: true,
+                  // ),
 
                   // Visibility(
                   //   visible:
@@ -328,11 +407,22 @@ class _HomeState extends State<Home> {
   // }
 
   // facebook add
+  // void _loadBannerAdd() {
+  //   facebookBannerAd = FacebookBannerAd(
+  //     placementId: Platform.isAndroid
+  //         ? "323745273316409_323745829983020"
+  //         : "1450991599021523_1450992009021482",
+  //     bannerSize: BannerSize.STANDARD,
+  //     listener: (result, vale) {
+  //       print("lister:");
+  //     },
+  //   );
+  // }
   void _loadBannerAdd() {
     facebookBannerAd = FacebookBannerAd(
       placementId: Platform.isAndroid
-          ? "323745273316409_323745829983020"
-          : "1450991599021523_1450992009021482",
+          ? "IMG_16_9_APP_INSTALL#323745273316409_323745829983020"
+          : "IMG_16_9_APP_INSTALL#1450991599021523_1450992009021482",
       bannerSize: BannerSize.STANDARD,
       listener: (result, vale) {
         print("lister:");
@@ -344,12 +434,14 @@ class _HomeState extends State<Home> {
     FacebookInterstitialAd.loadInterstitialAd(
       // placementId: "YOUR_PLACEMENT_ID",
       placementId: Platform.isAndroid
-          ? "323745273316409_323745926649677"
-          : "1450991599021523_1451005752353441",
+          ? "IMG_16_9_APP_INSTALL#323745273316409_323745926649677"
+          : "IMG_16_9_APP_INSTALL#1450991599021523_1451005752353441",
       listener: (result, value) {
         print(">> FAN > Interstitial Ad: $result --> $value");
-        if (result == InterstitialAdResult.LOADED)
+        if (result == InterstitialAdResult.LOADED) {
           _isInterstitialAdLoaded = true;
+          print('Add started ////');
+        }
 
         /// Once an Interstitial Ad has been dismissed and becomes invalidated,
         /// load a fresh Ad by calling this function.
@@ -361,4 +453,26 @@ class _HomeState extends State<Home> {
       },
     );
   }
+
+  // void _loadInterstitialAd() {
+  //   FacebookInterstitialAd.loadInterstitialAd(
+  //     // placementId: "YOUR_PLACEMENT_ID",
+  //     placementId: Platform.isAndroid
+  //         ? "323745273316409_323745926649677"
+  //         : "1450991599021523_1451005752353441",
+  //     listener: (result, value) {
+  //       print(">> FAN > Interstitial Ad: $result --> $value");
+  //       if (result == InterstitialAdResult.LOADED)
+  //         _isInterstitialAdLoaded = true;
+
+  //       /// Once an Interstitial Ad has been dismissed and becomes invalidated,
+  //       /// load a fresh Ad by calling this function.
+  //       if (result == InterstitialAdResult.DISMISSED &&
+  //           value["invalidated"] == true) {
+  //         _isInterstitialAdLoaded = false;
+  //         _loadInterstitialAd();
+  //       }
+  //     },
+  //   );
+  // }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,40 @@
+import 'dart:io';
+
 import 'package:ezeehome_webview/Contrlller/InternetConnectivity.dart';
 import 'package:ezeehome_webview/Screens/Home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
+  // await Permission.camera.request();
+  // await Permission.microphone.request();
+  // await Permission.storage.request();
+  //donot know much but got it from inappwebview offical documentation will check later
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+
+    var swAvailable = await AndroidWebViewFeature.isFeatureSupported(
+        AndroidWebViewFeature.SERVICE_WORKER_BASIC_USAGE);
+    var swInterceptAvailable = await AndroidWebViewFeature.isFeatureSupported(
+        AndroidWebViewFeature.SERVICE_WORKER_SHOULD_INTERCEPT_REQUEST);
+
+    if (swAvailable && swInterceptAvailable) {
+      AndroidServiceWorkerController serviceWorkerController =
+          AndroidServiceWorkerController.instance();
+
+      await serviceWorkerController
+          .setServiceWorkerClient(AndroidServiceWorkerClient(
+        shouldInterceptRequest: (request) async {
+          print(":::$request");
+          return null;
+        },
+      ));
+    }
+  }
   await checkInternetConnectionForDashboard();
   runApp(MyApp());
 }
@@ -35,41 +63,10 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: 
-        // MyWebViewApp()
-        Home(
+        home:
+            // MyWebViewApp()
+            Home(
           isInternetConnected: isInternetConnected,
-        )
-        );
+        ));
   }
 }
-// class MyWebViewApp extends StatefulWidget {
-//   @override
-//   _MyWebViewAppState createState() => _MyWebViewAppState();
-// }
-
-// class _MyWebViewAppState extends State<MyWebViewApp> {
-//   late InAppWebViewController _webViewController;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('My WebView App'),
-//       ),
-//       body: InAppWebView(
-//         initialUrlRequest: URLRequest(url: Uri.parse('https://twigo.date/')), // Replace with your website URL
-//         onWebViewCreated: (controller) {
-//           _webViewController = controller;
-//         },
-//         androidOnPermissionRequest: (controller, origin, resources) async {
-//           return PermissionRequestResponse(
-//             resources: resources,
-//             action: PermissionRequestResponseAction.GRANT, // Grant camera permission
-//           );
-//         },
-//       ),
-//     );
-//   }
-
-// }
